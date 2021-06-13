d30ce855fc8e68031e3cf64ca11df9f93168b9c4
Samandar Mazhardinov <msdeveloperuz@gmail.com>
2021-06-13 18:23:35 +0500

    dad jokes full project

---
+++ app.js
@@ -0,0 +1,37 @@
+const jokeEl = document.getElementById('joke')
+const jokeBtn = document.getElementById('jokeBtn')
+
+generateJoke()
+
+jokeBtn.addEventListener('click', generateJoke)
+
+
+// Using .then
+// function generateJoke() {
+//     const config = {
+//         headers: {
+//             Accept:'application/json'
+            
+//         }
+//     }
+
+//     fetch('https://icanhazdadjoke.com', config).then(res => res.json()).then(data => {
+//         jokeEl.textContent = data.joke
+//     })
+// }
+
+// using ASYc
+async function generateJoke() {
+    const config = {
+        headers: {
+            Accept:'application/json'
+            
+        }
+    }
+
+    const res = await fetch('https://icanhazdadjoke.com', config)
+
+    const data = await res.json();
+
+    jokeEl.textContent = data.joke
+}
